0781314c70299be164754f9dd4ac04122527210c
courtney-withers <courtneymwithers@gmail.com>
2023-08-20 22:39:31 +1000

    Added the selected feedback container

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,39 @@ from langchain.llms import OpenAI
 from streamlit_extras.stylable_container import stylable_container
 from streamlit_extras.stateful_button import button
 import pandas as pd
-import random
+from st_aggrid import AgGrid, GridUpdateMode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+
+################   FUNCTIONS  ######################
+def get_session_state():
+    session = st.session_state
+    if "checkbox_state" not in session:
+        session.checkbox_state = {}
+    return session
+
+# Define a function to update the checkbox state
+def update_checkbox_state(row):
+    session_state = get_session_state()
+    # print(f"Session state before update: {session_state}")
+    session_state.checkbox_state[row["Suggest Codes"]] = row["AorR"]
+    # print(f"Session state after update: {session_state}")
+
+# Define a function to get the checkbox state
+def get_checkbox_state():
+    session_state = get_session_state()
+    print(f"Get Checkbox Session state: {session_state}")
+    if "checkbox_state" not in session_state:
+        session_state.checkbox_state = {}
+    return session_state.checkbox_state
+
+# Define a function to clear the checkbox state
+def clear_checkbox_state():
+    session_state = get_session_state()
+    if "checkbox_state" in session_state:
+        del session_state.checkbox_state
+
+
+###########################################################
 
 st.set_page_config(page_title="🩺 Medical Billing")
 st.header('🩺 Medical Billing Codes Quickstart 🚑')
@@ -17,7 +49,7 @@ with st.sidebar:
     "## How to use\n"
     "1. Enter your medical short hand or clinical note into the top box 📝\n"  # noqa: E501
     "2. Press submit 👆🏽\n"
-    "3. Accept or Reject the suggest MBS billing codes ✅\n"
+    "3. Accept the suggested MBS billing codes ✅\n"
   )
   st.markdown("---")
   st.markdown("# About")
@@ -80,7 +112,7 @@ with stylable_container(
     
     st.markdown(
     "#### MBS Code Selection  \n"
-    "Click the buttons to accept or reject the relevant MBS codes"
+    "Click the checkboxes to accept the relevant MBS codes"
   )
     #PRINTING RETURNED MBS CODES
     #Here we want to print a list of the retunred MBS Codes
@@ -88,27 +120,52 @@ with stylable_container(
     # Generate a list of 10 random strings, each with a length of 5 characters
     random_MBS = ["MBS1234","MBS2345", "MBS6789", "MBS0798","MBS4692"]
 
-    #Create an array with the corresponding number of checkboxes
-    accept_reject_array = [False] * len(random_MBS)
+    #Create and array with procedures
+    procedure_array = ["Brain and Nervous System","Neck and Spine", "Blood", "Blood","Joint and Muscle"]
 
-    data_df = pd.DataFrame(
-        {
-            "Suggest Codes": random_MBS,
-            "AorR": accept_reject_array,
-        }
-    )
 
-    st.data_editor(
-        data_df,
-        column_config={
-            "AorR": st.column_config.CheckboxColumn(
-                "Click to Accept",
-                help="Click to Accept or Reject a Code",
-                default=False,
-            )
-        },
-        disabled=["Suggest Codes"],
-        hide_index=True,
-    )
+    # Apply the CheckboxColumn to the "AorR" column of the DataFrame
+#     st.data_editor(
+#         data_df,
+#         column_config={
+#             "AorR": st.column_config.CheckboxColumn(
+#                 "Click to Accept",
+#                 help="Click to Accept or Reject a Code",
+#                 default=False,
+#             )
+#         },
+#         disabled=["Suggest Codes"],
+#         hide_index=True,
+#     )
 
+# # Apply the update_checkbox_state function to each row in the DataFrame
+# data_df.apply(update_checkbox_state, axis=1)
+
+
+# # Get the checkbox state
+# checkbox_state = get_checkbox_state()
+
+# # Create an array with the checkbox values
+# checkbox_array = [checkbox_state.get(string, False) for string in random_MBS]
+
+# # Display the checkbox array
+# st.write(checkbox_array)
+
+######## TEST CODES #################
+    data = {
+        'MBS': random_MBS,
+        'Procedure': procedure_array
+    }
+
+    df = pd.DataFrame(data)
+    gd = GridOptionsBuilder.from_dataframe(df)
+    gd.configure_selection(selection_mode='multiple', use_checkbox=True)
+    gridoptions = gd.build()
+
+    grid_table = AgGrid(df, height=250, gridOptions=gridoptions,
+                        update_mode=GridUpdateMode.SELECTION_CHANGED)
+
+    st.write('## Selected')
+    selected_row = grid_table["selected_rows"]
+    st.dataframe(selected_row)
 
